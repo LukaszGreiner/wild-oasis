69ad42d7c7e04dc52bb3860407c34a7ea05ff7cb
xKaktus <greinerlukasz@gmail.com>
2024-11-18 18:42:18 +0100

    tailwind colors config

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>The wild oasis Hello!</h1>
+      <Link href="/cabins">Explorte luxury cabins</Link>
+    </div>
+  );
+}
