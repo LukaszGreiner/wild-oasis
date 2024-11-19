bd927a3a798117f0674b8126c08fc578d8f10f93
xKaktus <greinerlukasz@gmail.com>
2024-11-19 14:44:13 +0100

    Heder and nav

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Header from "./_components/Header";
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
@@ -24,13 +25,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl bg-red-500 mx-auto">{children}</main>
+        </div>
         <footer>&copy; Copyrighted by The Wild Oasis</footer>
       </body>
     </html>
