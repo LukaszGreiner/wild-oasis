f9e7562eeed5bfdd8f415862317eb6edff94bf9f
xKaktus <greinerlukasz@gmail.com>
2024-11-19 17:22:02 +0100

    Next Image

--- app/layout.jsx
+++ app/layout.jsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
         <Header />
 
         <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl bg-red-500 mx-auto">{children}</main>
+          <main className="max-w-7x mx-auto">{children}</main>
         </div>
         <footer>&copy; Copyrighted by The Wild Oasis</footer>
       </body>
